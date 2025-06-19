7bda9c2ce2c97cbcbe93c44b5051edd01b25c85e
BY8TECH <by8tech@gmail.com>
2025-06-19 16:38:09 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,18 @@
+// $(document).ready(function () {
+//     $('#certTable').DataTable();
+// });
+
+// $(document).ready(function () {
+//     $('#certTable').DataTable({
+//         responsive: true,
+//         renderer: 'semanticUI'
+//     });
+// });
+
+
+$(document).ready(function () {
+    $('#certTable').DataTable({
+        scrollX: true,
+        renderer: 'semanticUI'
+    });
+});
